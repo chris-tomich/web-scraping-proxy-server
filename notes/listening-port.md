3d333a65df4a654a624b283ac39568e63719853d
Chris Tomich <chris.tomich@gmail.com>
2020-08-16 17:29:32 +1000

    Basic working server

---
+++ notes/listening-port.md
@@ -0,0 +1,29 @@
+# What do I need to do?
+* Need to listen to the port.
+    * There's a skeleton of what needs to be done here
+        - https://www.geeksforgeeks.org/socket-programming-cc/
+* When I needed to figure out the size of a struct and casting the following references helped
+    - https://stackoverflow.com/questions/28273169/how-do-i-convert-between-numeric-types-safely-and-idiomatically
+    - https://doc.rust-lang.org/std/mem/fn.size_of.html
+* Need to fill in sockadd_in. Had to use the following references
+    - https://www.ict.griffith.edu.au/teaching/7421ICT/archive/guide/bgnet/output/html/sockaddr_inman.html
+    - https://man7.org/linux/man-pages/man2/socket.2.html
+    - https://man7.org/linux/man-pages/man2/getsockopt.2.html
+    - https://man7.org/linux/man-pages/man3/setsockopt.3p.html
+    - https://docs.rs/libc/0.2.74/libc/struct.sockaddr_in.html
+    - https://docs.rs/libc/0.2.74/libc/struct.in_addr.html
+    - https://users.rust-lang.org/t/help-understanding-libc-call/17308/9
+* For working out bind I was able to use some of the above plus the following
+    - https://docs.rs/libc/0.2.74/libc/fn.bind.html
+    - https://man7.org/linux/man-pages/man2/bind.2.html
+    * Just a note, it turns out when you're using bind for setting up a local port, you don't need to specify an address in sockadd_in. I just ended up trying 0 which is the same as the constant INADDR_ANY.
+    * I don't think you can listen on the loopback interface. I had trouble when I attempted to do it.
+* When I needed details on the error number returned from a failed call I used the following references
+    - https://docs.rs/libc/0.2.74/libc/fn.__errno_location.html
+        * So there was some learning about this done reading [this](https://www.reddit.com/r/rust/comments/bz6o1l/how_can_i_check_the_value_of_errno_without_libc/)
+        * Summary is, errno is a really sucky way of returning the last error in linux because C doesn't have multiple return values. As a result, the error doesn't necessarily exist until an error occurs so it may not exist.
+    - https://www-numi.fnal.gov/offline_software/srt_public_context/WebDocs/Errors/unix_system_errors.html
+* Used the following for listening to the socket.
+    - https://man7.org/linux/man-pages/man2/listen.2.html
+* Used the following for accepting an incoming connection.
+    - https://man7.org/linux/man-pages/man2/accept.2.html
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
+use libc;
+use std::str;
+use std::mem::size_of;
+
 fn main() {
-    println!("Hello, world!");
+    let file_descriptor : libc::c_int;
+    let set_socket_result : libc::c_int;
+    let bind_result : libc::c_int;
+    let listening_result : libc::c_int;
+    let new_socket : libc::c_int;
+
+    let mut socket_opt = 1;
+    let socket_opt_ptr = &mut socket_opt as *mut i32 as *mut libc::c_void;
+    let listening_port = 8080 as u16;
+
+    let mut buffer : [u8; 1024] = [0; 1024];
+
+    println!("listening port is {}", listening_port);
+
+    let socket_addr = libc::sockaddr_in{
+        sin_family: libc::AF_INET as u16,
+        sin_port: listening_port.swap_bytes(),
+        sin_addr: libc::in_addr{
+            s_addr: libc::INADDR_ANY
+        },
+        sin_zero: [0, 0, 0, 0, 0, 0, 0, 0]
+    };
+
+    let socket_addr_ptr = &socket_addr as *const libc::sockaddr_in as *const libc::sockaddr;
+
+    let mut socket_addr_peer = libc::sockaddr_in{
+        sin_family: libc::AF_INET as u16,
+        sin_port: listening_port.swap_bytes(),
+        sin_addr: libc::in_addr{
+            s_addr: libc::INADDR_ANY
+        },
+        sin_zero: [0, 0, 0, 0, 0, 0, 0, 0]
+    };
+
+    let socket_addr_peer_ptr = &mut socket_addr_peer as *mut libc::sockaddr_in as *mut libc::sockaddr;
+    let buffer_ptr = &mut buffer as *mut [u8; 1024] as *mut libc::c_void;
+
+    let mut socket_addr_size = size_of::<libc::socklen_t>() as u32;
+
+    unsafe {
+        // First I need to create a socket that can be used.
+        file_descriptor = libc::socket(libc::AF_INET, libc::SOCK_STREAM, 0);
+        set_socket_result = libc::setsockopt(file_descriptor, libc::SOL_SOCKET, libc::SO_REUSEADDR | libc::SO_REUSEPORT, socket_opt_ptr, size_of::<i32>() as u32);
+
+        println!("socket set result {}", set_socket_result);
+
+        bind_result = libc::bind(file_descriptor, socket_addr_ptr, size_of::<libc::sockaddr_in>() as u32);
+
+        println!("bind result {}", bind_result);
+
+        listening_result = libc::listen(file_descriptor, 3);
+
+        println!("listening result {}", listening_result);
+
+        new_socket = libc::accept(file_descriptor, socket_addr_peer_ptr, &mut socket_addr_size);
+
+        println!("accepted new connection {}", new_socket);
+
+        let amount_read = libc::read(new_socket, buffer_ptr, 1024);
+
+        println!("bytes read {} on port {}", amount_read, socket_addr_peer.sin_port);
+
+        println!("errno {}", *libc::__errno_location());
+    }
+
+    let result = String::from(str::from_utf8(&buffer).unwrap_or_default());
+    
+    println!("read the following from the client -\n{}", result);
 }
